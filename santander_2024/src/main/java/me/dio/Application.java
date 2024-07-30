a391df8eaaedd2fb70fe57e9c6a2efeac991a8ab
dayvson28 <dayvsonjeik@hotmail.com>
2024-07-29 22:07:30 -0300

    Implementação da camada de dominio

--- santander_2024/src/main/java/me/dio/Application.java
+++ santander_2024/src/main/java/me/dio/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Santander2024Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Santander2024Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
